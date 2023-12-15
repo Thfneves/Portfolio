43269d3dd33374016fe76ec0d4e7daa9ca54bea6
Thiago NeveS <140116209+Thfneves@users.noreply.github.com>
2023-12-15 13:21:47 -0300

    Add files via upload

--- src/componentes/elements/Cards.js
+++ src/componentes/elements/Cards.js
@@ -1,22 +1,33 @@
 import styles from "./card.module.css";
 import arquitet from "../../Image/portfolio/arquitet.svg";
 import ButtonB from "./ButtonB";
+import { useState } from "react";
 function Card({ img, title, tech, description, repo, site }) {
+  const [info, setInfo] = useState(false);
+
+  function InfoOf() {
+    setInfo(false);
+  }
+
+  function InfoOn() {
+    setInfo(true);
+  }
   return (
-    <div className={styles.card}>
-      <a href={site}>
+    <div onMouseLeave={InfoOf} className={styles.card}>
+      <a onMouseEnter={InfoOn} href={site}>
         <img src={img}></img>
       </a>
+      {info === true && (
+        <section>
+          <h3>{title}</h3>
+          <p>
+            <strong>Tecnologia</strong> {tech}
+          </p>
 
-      <section>
-        <h3>{title}</h3>
-        <p>
-          <strong>Tecnologia</strong> {tech}
-        </p>
-
-        <p>{description}</p>
-        <ButtonB text="Acesse o repositorio" link={repo}></ButtonB>
-      </section>
+          <p>{description}</p>
+          <ButtonB text="Acesse o repositorio" link={repo}></ButtonB>
+        </section>
+      )}
     </div>
   );
 }
--- src/componentes/section/Presentation.js
+++ src/componentes/section/Presentation.js
@@ -1,13 +1,48 @@
 import styles from "./Presentation.module.css";
-
+import { useEffect, useState } from "react";
 import ButtonA from "../elements/ButtonA";
+
 function Presentation() {
+  const [text, setText] = useState("");
+  const toRotate = ["Thiago Neves!", "Desenolvedor Front End"];
+  const [loop, setLoop] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const period = 100;
+  const [delta, setDelta] = useState(100);
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      toType();
+    }, delta);
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
+
+  const toType = () => {
+    let i = loop % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
+    setText(updatedText);
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setDelta(period);
+    } else if (isDeleting && updatedText === "") {
+      setIsDeleting(false);
+      setDelta(period);
+      setLoop(loop + 1);
+    }
+  };
+
   return (
     <div className={styles.Presentation}>
       <h4>
         <strong>Bem vindo ao meu portfolio</strong>
       </h4>
-      <h1>Ola me chamo Thiago Neves!</h1>
+      <h1>Ola eu sou {text}</h1>
       <p> Texto introdutorio</p>
       <ButtonA
         link="https://github.com/Thfneves"
